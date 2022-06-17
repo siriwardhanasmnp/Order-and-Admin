d08f8ddc89489ee6fbee47743b3b3c468c9d59b1
DeshinaWit <deshinawit@gmail.com>
2022-06-17 11:40:13 +0530

    Added dynamic table to tabs

--- src/pages/AgriMartFarmerOrderManagement/AgriMartFarmerOrderManagement.js
+++ src/pages/AgriMartFarmerOrderManagement/AgriMartFarmerOrderManagement.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AgriMartFarmerNavBar from '../../components/AgriMartFarmerNavBar/AgriMartFarmerNavBar';
 import './AgriMartFarmerOrderManagement.css';
 import AgriMartFooter from '../../components/AgriMartFooter/AgriMartFooter';
 import 'antd/dist/antd.css';
 import { Tabs } from 'antd';
+import { Button, Table } from 'react-bootstrap';
+import axios from 'axios';
+
 const { TabPane } = Tabs;
 
-
 function AgriMartFarmerOrderManagement() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // Update the document title using the browser API
+        apiCalls();
+    });
+
+    let apiCalls = (e) => {
+        try {
+
+            axios({
+                method: 'get',
+                url: 'http://localhost:8080/getproducts'
+            })
+                .then(res => {
+                    console.log('result', res);
+                    console.log('data', res.data);
+                    setData(res.data);
+                    console.log('data-array', data)
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <div>
             <div>
@@ -19,27 +51,149 @@ function AgriMartFarmerOrderManagement() {
                 <h2 class="txt-header">Orders Management</h2>
             </div>
             <div>
-            const App = () =&gt; (
+                const App = () =&gt; (
                 <div className="card-container">
                     <Tabs type="card">
                         <TabPane tab="Pending Orders" key="1">
-                            <p>Content of pending Orders</p>
+                            <div class="table-style">
+
+                                <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>Orders ID</th>
+                                            <th>Order Date</th>
+                                            <th>Product Name</th>
+                                            <th>Quantity</th>
+                                            <th>Price</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            data.map((item) => (
+                                                <tr key={item.id}>
+                                                    <td>{item.orderId}</td>
+                                                    <td>{item.orderDate}</td>
+                                                    <td>{item.orderName}</td>
+                                                    <td>{item.quantity}</td>
+                                                    <td>{item.price}</td>
+                                                    {/* <td>{item.price}</td> */}
+
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </Table>
+
+                            </div>
                         </TabPane>
                         <TabPane tab="Ready to Ship" key="2">
-                            <p>Content of RTS</p>
-                            
+                            <div class="table-style">
+
+                                <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>Orders ID</th>
+                                            <th>Order Date</th>
+                                            <th>Product Name</th>
+                                            <th>Quantity</th>
+                                            <th>Price</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            data.map((item) => (
+                                                <tr key={item.id}>
+                                                    <td>{item.orderId}</td>
+                                                    <td>{item.orderDate}</td>
+                                                    <td>{item.orderName}</td>
+                                                    <td>{item.quantity}</td>
+                                                    <td>{item.price}</td>
+                                                    {/* <td>{item.price}</td> */}
+
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </Table>
+
+                            </div>
+
                         </TabPane>
                         <TabPane tab="Shipped" key="3">
-                            <p>Shipped Orders</p>
-                            
+                            <div class="table-style">
+
+                                <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>Orders ID</th>
+                                            <th>Order Date</th>
+                                            <th>Product Name</th>
+                                            <th>Quantity</th>
+                                            <th>Price</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            data.map((item) => (
+                                                <tr key={item.id}>
+                                                    <td>{item.orderId}</td>
+                                                    <td>{item.orderDate}</td>
+                                                    <td>{item.orderName}</td>
+                                                    <td>{item.quantity}</td>
+                                                    <td>{item.price}</td>
+                                                    {/* <td>{item.price}</td> */}
+
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </Table>
+
+                            </div>
+
                         </TabPane>
                         <TabPane tab="Delivered" key="4">
-                            <p>Shipped Orders</p>
-                            
+                            <div class="table-style">
+
+                                <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>Orders ID</th>
+                                            <th>Order Date</th>
+                                            <th>Product Name</th>
+                                            <th>Quantity</th>
+                                            <th>Price</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            data.map((item) => (
+                                                <tr key={item.id}>
+                                                    <td>{item.orderId}</td>
+                                                    <td>{item.orderDate}</td>
+                                                    <td>{item.orderName}</td>
+                                                    <td>{item.quantity}</td>
+                                                    <td>{item.price}</td>
+                                                    {/* <td>{item.price}</td> */}
+
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </Table>
+
+                            </div>
+
                         </TabPane>
                     </Tabs>
                 </div>
             </div>
+
+
 
             <div>
                 <AgriMartFooter />
