44adecc5ca3d62078a074a33f1a3c37cd96045f7
DeshinaWit <deshinawit@gmail.com>
2022-06-17 10:46:40 +0530

    Added Tabs to Order Management

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FarmerPortalRoutes from './FarmerPortalRoutes';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/AgriMartFarmerOrderManagement/AgriMartFarmerOrderManagement.js
+++ src/pages/AgriMartFarmerOrderManagement/AgriMartFarmerOrderManagement.js
@@ -2,25 +2,53 @@ import React from 'react';
 import AgriMartFarmerNavBar from '../../components/AgriMartFarmerNavBar/AgriMartFarmerNavBar';
 import './AgriMartFarmerOrderManagement.css';
 import AgriMartFooter from '../../components/AgriMartFooter/AgriMartFooter';
-function AgriMartFarmerOrderManagement () {
+import 'antd/dist/antd.css';
+import { Tabs } from 'antd';
+const { TabPane } = Tabs;
+
+
+function AgriMartFarmerOrderManagement() {
     return (
         <div>
-        <div>
-            <AgriMartFarmerNavBar />
+            <div>
+                <AgriMartFarmerNavBar />
 
-        </div>
-        <div>
-  
-              <h2  class="txt-header">Orders Management</h2>
             </div>
-        <div>
-            <AgriMartFooter/>
+            <div>
+
+                <h2 class="txt-header">Orders Management</h2>
+            </div>
+            <div>
+            const App = () =&gt; (
+                <div className="card-container">
+                    <Tabs type="card">
+                        <TabPane tab="Pending Orders" key="1">
+                            <p>Content of pending Orders</p>
+                        </TabPane>
+                        <TabPane tab="Ready to Ship" key="2">
+                            <p>Content of RTS</p>
+                            
+                        </TabPane>
+                        <TabPane tab="Shipped" key="3">
+                            <p>Shipped Orders</p>
+                            
+                        </TabPane>
+                        <TabPane tab="Delivered" key="4">
+                            <p>Shipped Orders</p>
+                            
+                        </TabPane>
+                    </Tabs>
+                </div>
+            </div>
+
+            <div>
+                <AgriMartFooter />
+
+            </div>
 
         </div>
 
-    </div>
-
-      );
+    );
 }
 
 export default AgriMartFarmerOrderManagement;
